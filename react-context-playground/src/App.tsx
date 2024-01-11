e457c5462995bd535b793a560c772110f7acac9c
AthenasCode <emoore5203@gmail.com>
2024-01-11 18:25:14 +0800

    update readme link

--- react-context-playground/src/App.tsx
+++ react-context-playground/src/App.tsx
@@ -13,8 +13,11 @@ function App() {
         <h1>React Context Demo</h1>
         <p>
           This app uses themes to demonstrate React Context. Check the{" "}
-          <a href="www.github.com">readme.md file on Github</a> for further
-          explanation. I have used React Context to create various themes.
+          <a href="https://github.com/emoore29/React-Context-Playground#react-context-playground">
+            readme.md file on Github
+          </a>{" "}
+          for further explanation. I have used React Context to create various
+          themes.
         </p>
 
         <p>Current theme: "{theme}"</p>
